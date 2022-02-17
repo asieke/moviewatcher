787d2041aff8ab7ce2ded5b311167d713f4069e5
Alex Sieke <asieke@gmail.com>
2022-02-17 13:24:52 -0500

    added list view

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='flex flex-col px-10 py-10 items-center min-h-screen w-full bg-slate-700'>
+      <div className='flex mb-2 p-3'>
+        <Link href='/list'>List</Link>
+        &nbsp;|&nbsp;
+        <Link href='/'>Search</Link>
+      </div>
       <Component {...pageProps} />
     </div>
   );
---
+++ pages/list.tsx
@@ -0,0 +1,46 @@
+import { db } from '../lib/db';
+import { useState } from 'react';
+
+const List = ({ emoji }) => {
+  const [page, setPage] = useState<number>(1);
+  const perPage = 50;
+
+  console.log(db);
+  let disp = db.slice((page - 1) * perPage, page * perPage);
+
+  console.log(disp);
+
+  return (
+    <div className='text-center'>
+      <h1 className='text-5xl font-extrabold mb-6 text-slate-200'>
+        {emoji}
+        Top Movies
+        {emoji}
+      </h1>
+      <table className='justify-around mb-4 w-[250px] mx-auto text-slate-200 font-bold'>
+        <tr>
+          <td className='w-1/3'>{page > 1 && <a onClick={() => setPage(page - 1)}>Previous</a>}</td>
+          <td className='w-1/3'>Page {page}</td>
+          <td className='w-1/3'>
+            {page * perPage < db.length && <a onClick={() => setPage(page + 1)}>Next</a>}
+          </td>
+        </tr>
+      </table>
+
+      {disp.map((x, i) => {
+        return <p className='mb-3 text-xl text-slate-200' key={i}>{`${x.name} (${x.year})`}</p>;
+      })}
+    </div>
+  );
+};
+
+export default List;
+
+export async function getStaticProps() {
+  console.log(process.env);
+  return {
+    props: {
+      emoji: process.env.emoji,
+    },
+  };
+}
